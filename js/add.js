98901e3fe421488672259a734df34c5fecb6c4a1
rokas <rokasra@gmail.com>
2022-03-29 08:22:33 +0300

    cao

--- js/add.js
+++ js/add.js
@@ -1,5 +1,17 @@
-console.log("add");
 const baseUrl = "https://radial-reinvented-shoe.glitch.me";
+const formEl = document.querySelector("form");
+const addPropertyEl = document.querySelector(".add-property");
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newProperty = {
+    image: formEl.elements.image.value,
+    price: formEl.elements.price.value,
+    description: formEl.elements.description.value,
+    city: formEl.elements.city.value,
+  };
+  console.log("newProperty===", newProperty);
+});
 
 async function createProperty() {
   const res = await fetch(baseUrl, {
@@ -8,10 +20,10 @@ async function createProperty() {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      city: "Kaunas",
-      price: 50000,
-      image: "https://placeimg.com/640/480/any",
-      description: "this is good place",
+      city: "formEl.elements.city.value",
+      price: "formEl.elements.price.value",
+      image: "formEl.elements.image.value",
+      description: "formEl.elements.description.value",
     }),
   });
   const ats = await res.json();
